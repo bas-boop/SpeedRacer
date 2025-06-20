3678459c58da302a19a7dea3b42d7bd8f02838d4
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-06-20 23:16:17 +0200

    Changed main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,7 @@
-#include <iostream>
-
 #include "Gameplay/Game.h"
-#include "Math/Collider.h"
 
 int main()
-{
-	// Collider a;
-	// Collider b;
-	// Vector2 p = Vector2(0, 1);
-	//
-	// b.set_position(p);
-	//
-	// if (a.is_colliding(b))
-	// 	std::cout << "true\n";
-	// else
-	// 	std::cout << "false\n";
-	//
-	// return 0;
-	
+{	
 	Game game;
 	game.loop_game();
 }
